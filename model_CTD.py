05f27c3b21b2d5599caa525090519fbcacc0879c
niefca <corrine_nief@baylor.edu>
2014-08-08 15:31:50 -0400

    Update model_CTD.py

--- model_CTD.py
+++ model_CTD.py
@@ -4,7 +4,7 @@ log.verbose()    # request verbose output
 env = environ()  # create a new MODELLER environment to build this model in
 env.io.atom_files_directory = './:../atom_files'
 a = automodel(env,
-              alnfile  = 'newmodeller.pir', # must have this alignment file 
+              alnfile  = 'alignm_CTD.pir', # must have this alignment file 
               knowns   = ('d1c7ka_','PoingAA', ),
               sequence = 'query') #name of models
 a.starting_model= 1
