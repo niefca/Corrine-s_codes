de46f53486ce055aebb791dd34e1d0cf0366a56b
niefca <corrine_nief@baylor.edu>
2014-08-08 15:26:35 -0400

    Create model_NTD.py

---
+++ model_NTD.py
@@ -0,0 +1,12 @@
+from modeller import * 
+from modeller.automodel import * 
+log.verbose()    # request verbose output
+env = environ()  # create a new MODELLER environment to build this model in
+env.io.atom_files_directory = './:../atom_files'
+a = automodel(env,
+              alnfile  = 'newmodeller.pir',
+              knowns   = ('c4onsB_','c2g57A_','PoingAA', ),
+              sequence = 'query')
+a.starting_model= 1
+a.ending_model  = 50
+a.make()
