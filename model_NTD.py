86d9a95091f8420b19bf63aa95467211ba61a108
niefca <corrine_nief@baylor.edu>
2014-08-08 15:31:19 -0400

    Update model_NTD.py

--- model_NTD.py
+++ model_NTD.py
@@ -4,7 +4,7 @@ log.verbose()    # request verbose output
 env = environ()  # create a new MODELLER environment to build this model in
 env.io.atom_files_directory = './:../atom_files'
 a = automodel(env,
-              alnfile  = 'alignm_ntd.pir',
+              alnfile  = 'alignm_NTD.pir',
               knowns   = ('c4onsB_','c2g57A_','PoingAA', ),
               sequence = 'query')
 a.starting_model= 1
